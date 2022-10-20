93f93a23b6b83186cb9f09f3d18672812d96e7de
dmarcha <dmarcha@cpp.edu>
2022-10-20 15:30:21 -0700

    start working on random item picker func

--- billMain.c
+++ billMain.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 // pick random menu item
-
-// struct for the different menu items and their price
+int random_item_picker(){
+    srand(time(0));
+    // int and declare item and price
+    char item;
+    double price;
+    int i;
+    i = rand() % 4;
+    // this need to pick a random number from 0-3
+    // if number = 0 then item is salad
+    // if number = 1 then item is soup
+    // if number = 2 then item is sandwich
+    // if number = 3 then item is pizza
+    // retruns menu item and price
+    return 0;
+}
 
 // this is the main function
 int main(){
@@ -17,10 +31,10 @@ int main(){
 
     // ask the user what tax % 
     // ask user what tip %
-
+    random_item_picker();
     // perform calculations
-    // caculate tax = (menu_item x user_input/100)
-    // caculate tip = (menu_item x user_input/100)
+    // calculate tax = (menu_item x user_input/100)
+    // calculate tip = (menu_item x user_input/100)
     // add all together total = menu_item + tax + tip
     //print the total
     return 0;
