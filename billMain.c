dab2be890d6574ffce59a56f10d623cad00128ac
dmarcha <dmarcha@cpp.edu>
2022-10-20 13:41:48 -0700

    added outline

---
+++ billMain.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// pick random menu item
+
+// struct for the different menu items and their price
+
+// this is the main function
+int main(){
+    // init the value for the tax % and the tip %
+    // init these variables as doubles
+    // init and dec the variables for the menu item
+
+    // call on the function to pick a random menu item
+
+    // tell the user what the menu item is
+
+    // ask the user what tax % 
+    // ask user what tip %
+
+    // perform calculations
+    // caculate tax = (menu_item x user_input/100)
+    // caculate tip = (menu_item x user_input/100)
+    // add all together total = menu_item + tax + tip
+    //print the total
+    return 0;
+}
